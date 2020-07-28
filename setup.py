c95d97f4995c0f06710d26b08f883af0c47cddb5
mbe <mbe@pml.ac.uk>
2020-07-28 11:27:17 +0100

    Correct url in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='fvcom_river',
         version='0.1.0',
         description='Producing river data for FVCOM models',
-        url='git@gitlab.ecosystem-modelling.pml.ac.uk:mbe/FVCOM_rivers.git',
+        url='https://github.com/mikebedington/fvcom_rivers',
         author='mbe',
         author_email='mbe@pml.ac.uk',
         packages=['fvcom_river'],
